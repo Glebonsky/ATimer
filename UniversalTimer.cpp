bd7a86485b215401cdf6ce6022d1f3204c70305e
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-11-24 19:08:46 +0300

    Refactoring

--- UniversalTimer.cpp
+++ UniversalTimer.cpp
@@ -1,46 +1,40 @@
 #include "UniversalTimer.h"
+
+//returns millis from program start
+unsigned long millis();
+
+UniversalTimer::UniversalTimer(unsigned long interval)
+    : _interval(interval)
+{}
+
+void UniversalTimer::start()
+{
+    _running = true;
+    _lastTime = millis();
+}
+
+void UniversalTimer::setPeriod(unsigned long interval)
+{
+    _interval = interval;
+}
+
+bool UniversalTimer::check()
+{
+    if (!_running) return false;
+
+    if (millis() - _lastTime >= _interval)
+    {
+        _lastTime = millis(); 
+        return true;
+    }
+
+    return false;
+}
+
+void UniversalTimer::stop()
+{
+    _running = false;
+}
+
+
 #include <Arduino.h>
-//#ifdef GTimerRealTime
-//  #include <DS1307.h>
-//#endif
-  
-ATimer::ATimer(bool realTimeModule)
-{
-  _period = 0;
-  _lastTime = 0;
-  _running = false;
-//#ifdef GTimerRealTime
-//#endif
-
-}
-
-void ATimer::start()
-{
-  // if (_running) return;
-  
-  _running = true;
-  _lastTime = millis();
-}
-
-void ATimer::setPeriod(unsigned long period)
-{
-  _period = period;
-}
-
-bool ATimer::check()
-{
-  if (!_running) return false;
-
-  if (millis() - _lastTime >= _period)
-  {
-  _lastTime = millis(); 
-  return true;
-  }
-
-  return false;
-}
-
-void ATimer::stop()
-{
-  _running = false;
-}
--- UniversalTimer.h
+++ UniversalTimer.h
@@ -1,15 +1,16 @@
 #pragma once
 
-class ATimer
+class UniversalTimer
 {
-  unsigned long _period;
-  unsigned long _lastTime;
-  bool _running;
-  
 public:
-  ATimer(bool realTimeModule = false);
-  void start();
-  void setPeriod(unsigned long period);
-  bool check();   
-  void stop();
+    explicit UniversalTimer(unsigned long interval);
+    void start();
+    void setPeriod(unsigned long interval);
+    bool check();   
+    void stop();
+
+private:
+    unsigned long _interval = 0;
+    unsigned long _lastTime = 0;
+    bool _running = false;
 };
